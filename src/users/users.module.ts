1ddcf72dcc4a1d78ba27f8c97eabb72823e0f4c0
sebastian <sebastian.navarropiedra@ucreativa.com>
2026-06-27 00:29:31 -0600

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,14 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { User, UserSchema } from './schemas/users.schema';
+import { ProjectsModule } from 'projects/projects.module';
 
 @Module({
-
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema },
-  { name: User.name, schema: UserSchema }
-  ])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ProjectsModule, 
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
-export class UsersModule { }
+export class UsersModule {}
