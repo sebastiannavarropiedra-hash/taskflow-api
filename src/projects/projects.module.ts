93cff745751cb12212c301043cf70fbd9efb5438
sebastian <sebastian.navarropiedra@ucreativa.com>
2026-06-27 00:33:20 -0600

    Update projects.module.ts

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,12 +4,17 @@ import { ProjectsService } from './projects.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { Project, ProjectSchema } from './schemas/project.schema';
+import { Task, TaskSchema } from 'tasks/schemas/task.schema';
 
 
 @Module({
-
-  imports: [MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Project.name, schema: ProjectSchema },
+      { name: Task.name, schema: TaskSchema }, 
+    ]),
+  ],
   controllers: [ProjectsController],
-  providers: [ProjectsService]
+  providers: [ProjectsService],
 })
-export class ProjectsModule { }
+export class ProjectsModule {}
