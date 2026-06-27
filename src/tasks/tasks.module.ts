9fc001af9aee0139f68d6879d60810ece4898438
sebastian <sebastian.navarropiedra@ucreativa.com>
2026-06-27 01:21:36 -0600

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { Task, TaskSchema } from './schemas/task.schema';
-import { CommentSchema } from 'comments/schemas/comment.schema';
+import { CommentSchema,Comment } from 'comments/schemas/comment.schema';
 
 @Module({
   imports: [
